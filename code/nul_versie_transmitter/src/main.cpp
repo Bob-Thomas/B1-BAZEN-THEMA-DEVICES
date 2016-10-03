b6d78d0d9297a956fdf634f4db79508cf4f02ce6
Bob Thomas <bob_mathias_thomas@hotmail.com>
2016-10-03 16:20:27 +0200

    Added a send test for ir

--- code/nul_versie_transmitter/src/main.cpp
+++ code/nul_versie_transmitter/src/main.cpp
@@ -1,33 +1,81 @@
-#include "hwlib-due.hpp"
-int main() {
-    WDT->WDT_MR = WDT_MR_WDDIS;
+#include "bmptk.h"
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class send_ir : public rtos::task<> {
+    hwlib::target::d2_36kHz &ir;
+    hwlib::target::pin_in_out &button;
+    rtos::timer ir_timer;
+    char bits[16] = {'1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0'};
+
+    void main() {
+        for (; ;) {
+            bool clicked = !button.get();
+            while (clicked) {
+                for(int j = 0; j < 2; j++) {
+                    //start bit
+                    ir.set(1);
+                    ir_timer.set(1600*rtos::us);
+                    wait(ir_timer);
+
+                    ir.set(0);
+                    ir_timer.set(800*rtos::us);
+                    wait(ir_timer);
+
+                    //command
+                    for (int i = 0; i < 16; i++) {
+                        if (bits[i] == '0') {
+
+                            ir.set(1);
+                            ir_timer.set(800 * rtos::us);
+                            wait(ir_timer);
+
+                            ir.set(0);
+                            ir_timer.set(800 * rtos::us);
+                            wait(ir_timer);
+
+                        }
+                        if (bits[i] == '1') {
+                            ir.set(1);
+                            ir_timer.set(1600 * rtos::us);
+                            wait(ir_timer);
+
+                            ir.set(0);
+                            ir_timer.set(800 * rtos::us);
+                            wait(ir_timer);
+                        }
+                    }
+                    ir_timer.set(3 * rtos::ms);
+                    wait(ir_timer);
+                }
+                clicked = !button.get();
+            }
+        }
+    }
+
+public:
+    send_ir(
+            const char *name,
+            hwlib::target::d2_36kHz &ir,
+            hwlib::target::pin_in_out &button) : task(name), ir(ir), button(button), ir_timer(this, "ir_timer") {
+
+    }
+};
+
+int main(void) {
 
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
 
-    // wait for the PC terminal to start
-    hwlib::wait_ms( 500 );
+    // wait for the PC console to start
+    hwlib::wait_ms(500);
 
-    // IR output LED
-    auto ir = hwlib::target::d2_36kHz();
+    hwlib::cout << "timer demo\n";
 
-    // red output LED
-    auto red = hwlib::target::pin_out( hwlib::target::pins::d42 );
+    namespace target = hwlib::target;
+    auto ir = target::d2_36kHz();
+    auto button = target::pin_in_out(target::pins::d7);
+    auto ir_task = send_ir("ir_task", ir, button);
 
-
-    // switch which enables the 36 kHz output
-    auto sw = hwlib::target::pin_in( hwlib::target::pins::d43 );
-
-    // when the switch is pressed,
-    // repeat sending 1 ms signal and 1 ms silence
-    // and show this on the LED
-    for(;;){
-        hwlib::wait_ms( 1 );
-        ir.set( ! sw.get() );
-        red.set( ! sw.get() );
-
-        hwlib::wait_ms( 1 );
-        ir.set( 0 );
-    }
-
+    rtos::run();
 }
