f9fa6d17819179f585996076bc4a768b30d310c7
bob thomas <smartcat007@hotmail.com>
2016-11-09 14:38:15 +0100

    Reworked receiver again

--- code/proof of concept/src/tasks/receiver.cpp
+++ code/proof of concept/src/tasks/receiver.cpp
@@ -13,24 +13,11 @@ void Receiver::main() {
 //            amount_bits_found = 0;
 //        }
         idle();
-        (bits) = (bits <<1) | 1;
-        for(int i = 0; i < 16; i++) {
-            bool bit = ((bits >> (15-i))&1);
-            if(bit) {
-                hwlib::cout << "0";
-            } else {
-                hwlib::cout << "1";
-            }
-
-        }
-        hwlib::cout << "\n1000000101001010";
-        hwlib::cout << "\n##################3\n";
         Command c(bits);
         c.print_command();
-        hwlib::cout << "\n##################3\n";
         amount_bits_found = 0;
         bits = 0;
-        hwlib::wait_ms(2);
+        hwlib::wait_ms(3);
     }
 }
 
@@ -38,13 +25,19 @@ void Receiver::idle() {
     bool starting = true;
     while (starting) {
         if (signal.get() == 0) {
-            hwlib::wait_us(800);
+            hwlib::wait_us(400);
             if (signal.get() == 0) {
-                starting = false;
                 hwlib::wait_us(800);
-                while (amount_bits_found < 16) {
-                    signal_found();
+                if (signal.get() == 0) {
                     hwlib::wait_us(800);
+                    //start bit found
+                    bits &= ~(1 << (max_bits));
+                    hwlib::cout << "1";
+                    amount_bits_found++;
+                    starting = false;
+                    while (amount_bits_found < 16) {
+                        signal_found();
+                    }
                 }
             }
 
@@ -53,19 +46,21 @@ void Receiver::idle() {
 }
 
 void Receiver::signal_found() {
-    bool in_progress = true;
-    if (signal.get() == 0) {
-        hwlib::wait_us(800);
-        amount_bits_found++;
+    if(signal.get() == 0) {
+        hwlib::wait_us(1200);
         if (signal.get() == 1) {
-            in_progress = false;
-            bits  |= (1 << (max_bits-amount_bits_found));
-        } else if (signal.get() == 0 && in_progress) {
-            bits  |= (0 << (max_bits-amount_bits_found));
+            bits |= (1 << (max_bits - amount_bits_found));
+            amount_bits_found++;
+            hwlib::cout << "0";
+
+        } else if (signal.get() == 0) {
+            bits &= ~(1 << (max_bits - amount_bits_found));
+            amount_bits_found++;
+            hwlib::cout << "1";
+
         }
+        hwlib::wait_us(600);
     }
-
-
 }
 //1 10000 00101 00101
 //1 00000 01010 01010
